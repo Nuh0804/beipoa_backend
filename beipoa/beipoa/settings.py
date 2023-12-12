4e58144fce2e5ad99d23a8216748910826c65177
Nuh0804 <nuh.saidi.buhero@gmail.com>
2023-12-12 22:51:48 +0300

    database change

--- beipoa/beipoa/settings.py
+++ beipoa/beipoa/settings.py
@@ -82,15 +82,10 @@ WSGI_APPLICATION = 'beipoa.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'beipoa',
-        'HOST': 'localhost',
-        'USER': 'postgres',
-        'PASSWORD': 'nuhusaidi',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
